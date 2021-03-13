ea75a76536990eae35ad563cd9c62f56127e1dbe
sreeb <you@example.com>
2021-03-13 11:55:56 +0530

    third commit

--- Firstpython/urls.py
+++ Firstpython/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+from django.contrib import adminbfdnzzzzzzzzzzzzzzgbggfnfgmfgfngng
 from django.urls import path
 from django.urls import include,path
 
